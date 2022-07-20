8a7115d01353b639ad925b92b7c5740680979b73
zhou.xihuan <1325193247@qq.com>
2022-07-20 17:52:06 +0800

    cookie处理、登出

---
+++ savage-ui/src/views/lava/Menu.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="index">
+    <h1>menu</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu"
+}
+</script>
+
+<style scoped>
+
+</style>
--- savage-ui/src/views/lava/User.vue
+++ savage-ui/src/views/lava/User.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="index">
-    <h1>This is an manage page</h1>
+    <h1>user</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'manage',
+  name: 'User',
 }
 </script>
 
